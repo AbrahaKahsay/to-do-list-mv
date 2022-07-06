c7b0e94f04483e2c8dce150337d928cd8ef1a17e
Abraha Kahsay <abrahak81@gmail.com>
2022-07-06 13:10:50 +0300

    Add tasks array of objects in index.js file

--- src/index.js
+++ src/index.js
@@ -2,25 +2,24 @@ import _ from 'lodash';
 import './style.css';
 // import Icon from './icon.png';
 
-// function component() {
-//     const element = document.createElement('div');
-  
-//    // Lodash, now imported by this script
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//     element.classList.add('hello');
+const form = document.getElementById('form');
+const tasks = [
+    {
+        description: 'attending mornning session',
+        completed: true,
+        index: 0,
 
-//     // Add the image to our existing div.
-
-//   const myIcon = new Image();
-
-//   myIcon.src = Icon;
-
-
-//   element.appendChild(myIcon);
-
-
-  
-//     return element;
-//   }
-  
-//   document.body.appendChild(component());
+    },
+    {
+        description: 'coding',
+        completed: true,
+        index: 1,
+    
+    },
+    {
+        description: 'break dance',
+        completed: false,
+        index: 0,
+    
+    },
+];
