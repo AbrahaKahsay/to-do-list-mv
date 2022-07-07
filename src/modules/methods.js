b51618fb9214a2db72e3ceb90f6928596e75c091
Abraha Kahsay <abrahak81@gmail.com>
2022-07-07 15:16:41 +0300

    Create DOM handlers in methods.js

---
+++ src/modules/methods.js
@@ -0,0 +1,8 @@
+import Dots from './modules/dots.png';
+import Trash from './modules/trash-bin.png';
+import Edit from './modules/edit.png';
+
+// handles DoM elements
+const addList = document.getElementById('add-list');
+const newList = document.getElementById('new-list');
+
