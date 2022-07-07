0d65ed7a1993d9ce76b146a794f61b97ce9925c5
Abraha Kahsay <abrahak81@gmail.com>
2022-07-07 14:45:39 +0300

    Refactor index.html

---
+++ src/modules/classes.js
@@ -0,0 +1,7 @@
+export default class Todos {
+    constructor(description, complete, index) {
+      this.description = description;
+      this.complete = complete;
+      this.index = index;
+    }
+  }
