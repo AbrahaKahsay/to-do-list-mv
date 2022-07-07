172180859440db1a64d697c8b92783a6144d0c32
Abraha Kahsay <abrahak81@gmail.com>
2022-07-07 15:37:06 +0300

    Create a div to store form inputs

--- src/modules/methods.js
+++ src/modules/methods.js
@@ -13,3 +13,20 @@ class Tasks {
     }
 }
 
+// create display method to show on the DoM
+const display = ()=>{
+    if(localStorage.Tasks) {
+        for(let i=0; i<JSON.parse(localStorage.Tasks).length; i+=1) {
+            // creating a div element to store form inputs
+            const div = document.createElement('div');
+            const newCheck = document.createElement('input');
+            const newLabel = document.createElement('input');
+            const lineBreak = document.createElement('br');
+            const line = document.createElement('hr');
+            const dotsIcon = new Image();
+            const trashIcon = new Image();
+            const editIcon = new Image();
+            const todo = document.createElement('div');
+        }
+    }
+}
