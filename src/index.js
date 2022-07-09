df10fd9ff849c8c5f0b2808fd96a99d6ad15f184
Abraha Kahsay <abrahak81@gmail.com>
2022-07-09 18:57:58 +0300

    Make changes on index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // import _ from 'lodash';
-import './style.css';
-import Refresh from './refresh.png';
-import Enter from './enter.png';
-import Todos from './classes.js';
-import { Tasks, addList, display } from './methods.js';
+import "./style.css";
+import Refresh from "./refresh.png";
+import Enter from "./enter.png";
+import Todos from "./classes.js";
+import { Tasks, addList, display } from "./methods.js";
 
 // creating a todo list collection by instantiating Tasks class
 const collection = new Tasks();
@@ -14,16 +14,17 @@ if (localStorage.Tasks) {
 }
 
 // Add event to add list
-addList.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
+addList.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
     const newTasks = new Todos();
     newTasks.description = addList.value;
     newTasks.complete = false;
     newTasks.index = `${collection.array.length + 1}`;
     collection.array.push(newTasks);
-    newTasks.value = '';
+    console.log(collection.array);
+    newTasks.value = "";
     const stringData = JSON.stringify(collection.array);
-    localStorage.setItem('Tasks', stringData);
+    localStorage.setItem("Tasks", stringData);
     window.location.reload();
   }
 });
@@ -37,10 +38,12 @@ for (let i = 0; i < collection.array.length; i += 1) {
   const descriptionName = collection.array[i].description;
   const deleteItem = document.getElementById(`item${i}`);
   // Add event listner
-  remove.addEventListener('click', () => {
-    const filtered = collection.array.filter((Tasks) => Tasks.description !== descriptionName);
+  remove.addEventListener("click", () => {
+    const filtered = collection.array.filter(
+      (Tasks) => Tasks.description !== descriptionName
+    );
     const stringData = JSON.stringify(filtered);
-    localStorage.setItem('Tasks', stringData);
+    localStorage.setItem("Tasks", stringData);
     deleteItem.remove();
     window.location.reload();
   });
@@ -49,21 +52,22 @@ for (let i = 0; i < collection.array.length; i += 1) {
 // update index
 const updateIndex = () => {
   collection.array.forEach((todos, index) => {
-    todos.index = index + 1;
+    todos.index = index;
   });
   const stringData = JSON.stringify(collection.array);
-  localStorage.setItem('Tasks', stringData);
+  localStorage.setItem("Tasks", stringData);
 };
 updateIndex();
 
 // Edit and update tasks in the localStorage;
 const updateTask = () => {
-  const list = document.getElementsByClassName('list');
+  const list = document.getElementsByClassName("list");
+  // console.log(list);
   for (let i = 0; i < list.length; i += 1) {
-    list[i].addEventListener('change', () => {
+    list[i].addEventListener("change", () => {
       collection.array[i].description = list[i].value;
       const stringData = JSON.stringify(collection.array);
-      localStorage.setItem('Tasks', stringData);
+      localStorage.setItem("Tasks", stringData);
       window.location.reload();
     });
   }
@@ -73,64 +77,75 @@ updateTask();
 // Add functionality to checkbox
 for (let i = 0; i < collection.array.length; i += 1) {
   const checkbox = document.getElementById(`box${i}`);
-  checkbox.addEventListener('change', () => {
+  checkbox.addEventListener("change", () => {
     if (collection.array[i].complete === false) {
       collection.array[i].complete = true;
-      localStorage.setItem('Tasks', JSON.stringify(collection.array));
+      localStorage.setItem("Tasks", JSON.stringify(collection.array));
 
       const list = document.getElementById(`list${i}`);
-      list.style.textDecoration = 'line-through';
+      list.style.textDecoration = "line-through";
     } else if (collection.array[i].complete === true) {
       collection.array[i].complete = false;
-      localStorage.setItem('Tasks', JSON.stringify(collection.array));
+      localStorage.setItem("Tasks", JSON.stringify(collection.array));
 
       const lists = document.getElementById(`list${i}`);
-      lists.style.textDecoration = 'none';
+      lists.style.textDecoration = "none";
     }
   });
 }
 
 // Add functionality to clear button
-const clear = document.getElementById('clearBtn');
-clear.addEventListener('click', () => {
-  // filtering out incomplete tasks
-  const filtered = collection.array.filter((items) => items.complete === false);
-  const stringData = JSON.stringify(filtered);
+const clear = document.getElementById("clearBtn");
+clear.addEventListener("click", () => {
   for (let i = 0; i < collection.array.length; i += 1) {
-    const markedItem = document.getElementById(`item${i}`);
-    markedItem.remove();
+    // console.log(collection.array[i].complete);
+    if (collection.array[i].complete) {
+      const markedItem = document.getElementById(`item${i}`);
+      // console.log(markedItem);
+      markedItem.remove();
+    }
   }
-  // store filtered items into local storage
-  localStorage.setItem('Tasks', stringData);
 
-  const remove = document.querySelector('.trashIcon');
-  remove.style.display = 'none';
+  // filtering out incomplete tasks
+  collection.array = collection.array.filter(
+    (items) => items.complete === false
+  );
+  // console.log(filtered);
+  const stringData = JSON.stringify(collection.array);
+  // console.log(stringData);
+
+  // store filtered items into local storage
+  localStorage.setItem("Tasks", stringData);
+
+  const remove = document.querySelector(".trashIcon");
+  remove.style.display = "none";
 
   updateTask();
   updateIndex();
+  window.location.reload();
 });
 
 // Add the refresh button
 // handle and add to DOM
-const refresher = document.getElementById('title');
+const refresher = document.getElementById("title");
 const refresh = new Image();
 refresh.src = Refresh;
-refresh.id = 'refresh';
-refresh.className = 'refresh';
+refresh.id = "refresh";
+refresh.className = "refresh";
 refresher.appendChild(refresh);
 
-const reloadPage = document.querySelector('#refresh');
+const reloadPage = document.querySelector("#refresh");
 // Reload everything:
 function reload() {
   window.location.reload();
 }
 // Event listeners for reload
-reloadPage.addEventListener('click', reload, false);
+reloadPage.addEventListener("click", reload, false);
 
 // Add eneter key
-const form = document.getElementById('list');
+const form = document.getElementById("list");
 const enter = new Image();
 enter.src = Enter;
-enter.className = 'enter';
-enter.id = 'enter';
+enter.className = "enter";
+enter.id = "enter";
 form.appendChild(enter);
