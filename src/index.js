edca41070d7c74490f73794025c19e2f3a7b86f0
Abraha Kahsay <abrahak81@gmail.com>
2022-07-14 07:21:23 +0300

    Refactor functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-// import _ from 'lodash';
 import './style.css';
 import Refresh from './refresh.png';
 import Enter from './enter.png';
-import Todos from './classes.js';
-import { Tasks, addList, display } from './methods.js';
+import Todos from './modules/classes.js';
+import { Tasks, collection, addData, removeData, updateIndex, updateTask,
+  clearStorage, clearAllComplete, display, addList } from './modules/methods.js';
 
-// creating a todo list collection by instantiating Tasks class
-const collection = new Tasks();
 
-// check local storage and update
-if (localStorage.Tasks) {
-  collection.array = JSON.parse(localStorage.Tasks);
-}
+addData();
 
 // Add event to add list
 addList.addEventListener('keypress', (event) => {
@@ -21,7 +16,7 @@ addList.addEventListener('keypress', (event) => {
     newTasks.complete = false;
     newTasks.index = `${collection.array.length + 1}`;
     collection.array.push(newTasks);
-    newTasks.value = '';
+    newTasks.value = ' ';
     const stringData = JSON.stringify(collection.array);
     localStorage.setItem('Tasks', stringData);
     window.location.reload();
@@ -30,99 +25,11 @@ addList.addEventListener('keypress', (event) => {
 
 // display the todos added
 display();
-
-// remove item from collection
-for (let i = 0; i < collection.array.length; i += 1) {
-  const remove = document.getElementById(`remove${i}`);
-  const descriptionName = collection.array[i].description;
-  const deleteItem = document.getElementById(`item${i}`);
-  // Add event listner
-  remove.addEventListener('click', () => {
-    const filtered = collection.array.filter(
-      (Tasks) => Tasks.description !== descriptionName,
-    );
-    const stringData = JSON.stringify(filtered);
-    localStorage.setItem('Tasks', stringData);
-    deleteItem.remove();
-    window.location.reload();
-  });
-}
-
-// update index
-const updateIndex = () => {
-  collection.array.forEach((todos, index) => {
-    todos.index = index;
-  });
-  const stringData = JSON.stringify(collection.array);
-  localStorage.setItem('Tasks', stringData);
-};
+removeData();
 updateIndex();
-
-// Edit and update tasks in the localStorage;
-const updateTask = () => {
-  const list = document.getElementsByClassName('list');
-  for (let i = 0; i < list.length; i += 1) {
-    list[i].addEventListener('change', () => {
-      collection.array[i].description = list[i].value;
-      const stringData = JSON.stringify(collection.array);
-      localStorage.setItem('Tasks', stringData);
-      window.location.reload();
-    });
-  }
-};
 updateTask();
-
-// Add functionality to checkbox
-for (let i = 0; i < collection.array.length; i += 1) {
-  const checkbox = document.getElementById(`box${i}`);
-  checkbox.addEventListener('change', () => {
-    if (collection.array[i].complete === false) {
-      collection.array[i].complete = true;
-      localStorage.setItem('Tasks', JSON.stringify(collection.array));
-
-      const list = document.getElementById(`list${i}`);
-      list.style.textDecoration = 'line-through';
-    } else if (collection.array[i].complete === true) {
-      collection.array[i].complete = false;
-      localStorage.setItem('Tasks', JSON.stringify(collection.array));
-
-      const lists = document.getElementById(`list${i}`);
-      lists.style.textDecoration = 'none';
-    }
-  });
-}
-
-// Add functionality to clear button
-const clear = document.getElementById('clearBtn');
-clear.addEventListener('click', () => {
-  for (let i = 0; i < collection.array.length; i += 1) {
-    // console.log(collection.array[i].complete);
-    if (collection.array[i].complete) {
-      const markedItem = document.getElementById(`item${i}`);
-      // console.log(markedItem);
-      markedItem.remove();
-    }
-  }
-
-  // filtering out incomplete tasks
-  collection.array = collection.array.filter(
-    (items) => items.complete === false,
-  );
-  // console.log(filtered);
-  const stringData = JSON.stringify(collection.array);
-  // console.log(stringData);
-
-  // store filtered items into local storage
-  localStorage.setItem('Tasks', stringData);
-
-  const remove = document.querySelector('.trashIcon');
-  remove.style.display = 'none';
-
-  updateTask();
-  updateIndex();
-  window.location.reload();
-});
-
+clearStorage();
+clearAllComplete();
 // Add the refresh button
 // handle and add to DOM
 const refresher = document.getElementById('title');
@@ -132,12 +39,11 @@ refresh.id = 'refresh';
 refresh.className = 'refresh';
 refresher.appendChild(refresh);
 
+// Loading the page for refresher
 const reloadPage = document.querySelector('#refresh');
-// Reload everything:
 function reload() {
   window.location.reload();
 }
-// Event listeners for reload
 reloadPage.addEventListener('click', reload, false);
 
 // Add eneter key
